af70c2f6522e00167463e2fcbc2aba01c6727daf
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-26 22:17:49 +0700

    added user crud

---
+++ app/Repository/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repository\User;
+
+use App\Models\User;
+
+class UserRepository
+{
+    public function getAll()
+    {
+        return User::all();
+    }
+
+    public function getById($id)
+    {
+        return User::find($id);
+    }
+
+    public function create($request)
+    {
+        return User::create($request);
+    }
+
+    public function update($id, $request)
+    {
+        $user = $this->getById($id);
+        return $user->update($request);
+    }
+
+    public function delete($id)
+    {
+        return $this->getById($id)->delete();
+    }
+}
