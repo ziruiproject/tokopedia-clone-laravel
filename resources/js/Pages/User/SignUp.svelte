260f42bfd25b6d7e2792aa694012a8daaa3db345
Zirui Project <88216794+ziruiproject@users.noreply.github.com>
2023-12-27 08:51:50 +0700

    building signup pages

---
+++ resources/js/Pages/User/SignUp.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Sign Up</title>
+</svelte:head>
+
+<main>
+    <h1>Halo</h1>
+    <form action="">
+        <input type="text" />
+        <input type="password" />
+    </form>
+</main>
